19c8fdfc32ee0bb3893f335bd3d08c065afc7bd4
lissa-vet <125082940+lissa-vet@users.noreply.github.com>
2023-03-04 14:08:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -21,7 +21,9 @@ double pown(double value, uint16_t n) {
 uint64_t fact(uint16_t n) {
   if (!n) {
     return 1;
-  } else return n*fact(n - 1);
+  } else {
+    return n*fact(n - 1);
+  }
 }
 
 double calcItem(double x, uint16_t n) {
